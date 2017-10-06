fde4037d8be12672f76a29d5b10bd8c8250134cd
Matt Lewis <matt@mplewis.com>
2017-10-06 16:21:42 -0600

    queries with current_user

---
+++ app/graphql/types/line_item_type.rb
@@ -0,0 +1,22 @@
+Types::LineItemType = GraphQL::ObjectType.define do
+  name 'LineItem'
+
+  field :id, !types.Int
+  field :created_at, !types.String
+  field :updated_at, !types.String
+
+  field :txn_type, !types.String
+  field :status, !types.String
+
+  field :date, !types.String
+  field :cents, !types.Int
+  field :account, !types.String
+  field :category, !types.String
+
+  field :desc, types.String
+  field :desc_orig, types.String
+  field :note, types.String
+
+  field :creator, !Types::UserType
+  field :assignee, !Types::UserType
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,29 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :allUsers, !types[Types::UserType] do
     description 'All users'
-    resolve ->(obj, args, ctx) {
+    resolve ->(_obj, _args, _ctx) do
       User.all
-    }
+    end
+  end
+
+  field :allLineItems, !types[Types::LineItemType] do
+    description 'All line items'
+    resolve ->(_obj, _args, _ctx) do
+      LineItem.all
+    end
+  end
+
+  field :lineItemsCreatedByMe, !types[Types::LineItemType] do
+    description 'Line items assigned to the current user'
+    resolve ->(_obj, _args, ctx) do
+      LineItem.where creator: ctx[:current_user]
+    end
+  end
+
+  field :lineItemsAssignedToMe, !types[Types::LineItemType] do
+    description 'Line items assigned to the current user'
+    resolve ->(_obj, _args, ctx) do
+      LineItem.where assignee: ctx[:current_user]
+    end
   end
 end
