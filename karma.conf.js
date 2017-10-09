f0bbf6d7090dc7109c95e75da8228657cbf1312b
Matt Lewis <matt@mplewis.com>
2017-10-09 13:38:51 -0600

    spec: ButtonSelect

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,22 @@
 const webpack = require('./config/webpack/test.js')
 
-const specs = ['spec/javascript/**/*.js', 'spec/javascript/**/*.jsx']
+const specs = ['spec/javascript/**/*_spec.js', 'spec/javascript/**/*_spec.jsx']
 
 const files = specs.map(glob => ({ pattern: glob, watched: false }))
 const preprocessors = {}
 specs.forEach(glob => (preprocessors[glob] = ['webpack', 'sourcemap']))
+
+const mocha = {
+  ui: 'bdd-lazy-var/getter',
+  require: [require.resolve('bdd-lazy-var/bdd_lazy_var_getter')]
+}
 
 module.exports = function (config) {
   config.set({
     reporters: ['mocha'],
     frameworks: ['mocha', 'sinon-chai'],
     browsers: ['jsdom'],
+    client: { mocha },
     files,
     preprocessors,
     webpack
--- spec/javascript/components/button_select_spec.jsx
+++ spec/javascript/components/button_select_spec.jsx
@@ -3,18 +3,36 @@ import { mount } from 'avoriaz'
 import ButtonSelect from 'components/button_select'
 
 describe('ButtonSelect', function () {
-  it('works', function () {
-    const comp = mount(ButtonSelect, {
+  subject(() =>
+    mount(ButtonSelect, {
       propsData: {
-        stateChanged: console.log,
+        stateChanged: get.stateChanged,
         buttons: [
           { value: 'hello', name: 'Hello!', color: 'success' },
           { value: 'goodbye', name: 'Goodbye.', color: 'danger' }
         ]
       }
     })
-    console.log(comp.html())
-    console.log(comp.first('button').html())
-    expect(comp).to.exist
+  )
+  function buttonByText (text) {
+    return subject()
+      .find('button')
+      .filter(button => button.text().includes(text))[0]
+  }
+  def('stateChanged', sinon.spy)
+  def('hello', () => buttonByText('Hello!'))
+  def('goodbye', () => buttonByText('Goodbye.'))
+
+  it('maintains state and fires callbacks', function () {
+    get.hello.trigger('click')
+    get.goodbye.trigger('click')
+    get.hello.trigger('click')
+    get.hello.trigger('click')
+    expect(get.stateChanged.args).to.eql([
+      ['hello'],
+      ['goodbye'],
+      ['hello'],
+      [null]
+    ])
   })
 })
