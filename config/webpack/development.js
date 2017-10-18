7d2adaafbb4c27fe4fb19300a3198fce3e869387
Matt Lewis <matt@mplewis.com>
2017-10-17 21:02:07 -0600

    dev: hot reloading

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,16 @@
 const environment = require('./environment')
 
+const babel = environment.loaders.get('babel')
+babel.test = /\.(js)?(\.erb)?$/
+
+const babelJsx = Object.assign({}, babel)
+babelJsx.test = /\.(jsx)?(\.erb)?$/
+babelJsx.use = [
+  { loader: babelJsx.loader, options: babelJsx.options },
+  'vue-jsx-hot-loader'
+]
+delete babelJsx.loader
+delete babelJsx.options
+environment.loaders.set('babelJsx', babelJsx)
+
 module.exports = environment.toWebpackConfig()
