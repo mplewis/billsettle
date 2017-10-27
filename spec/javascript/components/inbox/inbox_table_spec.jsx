6b4c9d6bc5c5da1fca32e0c5dce544bf14d0d562
Matt Lewis <matt@mplewis.com>
2017-10-26 19:04:09 -0600

    spec: inbox table

---
+++ spec/javascript/components/inbox/inbox_table_spec.jsx
@@ -0,0 +1,44 @@
+import IncompleteTable from 'components/incomplete/incomplete_table'
+
+const lineItems = [11, 12, 13].map(i => ({
+  id: i,
+  creator: {
+    id: 1,
+    email: 'matt@mplewis.com'
+  },
+  date: '2017-10-15T18:48:00.488Z',
+  cents: 218145,
+  desc: 'Apple Store Cherry Creek',
+  desc_orig: 'APPLE STORE CCK**',
+  category: 'debit',
+  account: 'Chase',
+  debt_owner: 'split'
+}))
+
+describe('IncompleteTable', function () {
+  subject(() =>
+    mount(IncompleteTable, {
+      propsData: {
+        lineItems,
+        stateChanged: get('stateChanged')
+      }
+    })
+  )
+  def('assignDebtToMe', () => subject().first('button'))
+  def('stateChanged', sinon.spy)
+
+  it('renders line items', function () {
+    expect(subject().text()).to.include('Apple Store Cherry Creek')
+    expect(subject().text()).to.include('APPLE STORE CCK**')
+    expect(subject().text()).to.include('$2,181.45')
+    expect(subject().find('tbody tr').length).to.eq(3)
+  })
+
+  context('when clicking a button', function () {
+    beforeEach(() => get('assignDebtToMe').trigger('click'))
+
+    it('calls stateChanged', function () {
+      expect(get('stateChanged')).to.be.calledWith(lineItems[0], 'creator')
+    })
+  })
+})
